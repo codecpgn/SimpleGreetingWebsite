e271c23eef55b00fff0d0e1dae883bdd479d8ca5
surya <cpgnsc@gmail.com>
2022-08-19 22:54:17 +0545

    adding CSS

--- src/index.js
+++ src/index.js
@@ -5,23 +5,26 @@ import "./index.css" //for monitoring css
 let currDate = new Date();
 currDate= currDate.getHours();
 let greeting ='';
-//const cssStyle ={
- // color : 'green'
-//};
+
+const cssStyle ={}
+;
 
 if(currDate>=1 && currDate <12){
   greeting="Good morning";
+  cssStyle.color= "yellow";
 }
 else if (currDate>=12 && currDate<19){
   greeting ="Good Afternoon ";
+  cssStyle.color= "red";
 }else{
   greeting ="Good Night"
+  cssStyle.color= "orange";
 }
 
 
 ReactDOM.render(
   <>
-<h1>hello guys, {greeting}</h1>
+<h1>Hello Guys, <span style={cssStyle}>{greeting}</span></h1>
 </>,
 document.getElementById("root")
 );
